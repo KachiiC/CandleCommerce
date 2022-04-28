b1cb8fa42da9e96c99dcd8899cf5f4373faf44ec
KachiiC <kacheokcheong@gmail.com>
2022-04-28 15:58:17 +0100

    added Home Button

---
+++ client/src/components/HomeButton.js
@@ -0,0 +1,10 @@
+import { Link } from 'react-router-dom'
+
+export const HomeButton = () => (
+    <Link to="/">
+        <button className='home_button'>
+            Home
+        </button>
+    </Link>
+)
+
