2eb2e48f302fa09a1e9266044e80081734390deb
KachiiC <kacheokcheong@gmail.com>
2022-04-28 15:58:52 +0100

    added helper funcs

--- client/src/helpers/index.js
+++ client/src/helpers/index.js
@@ -1,1 +1,13 @@
+import moment from 'moment'
+
 export const TotalAmount = input => input.reduce((prev, cur) => prev + cur).toFixed(2)
+
+export const Compare = (a, b) => {
+    if (b.date > a.date) return 1;
+    if (b.date < a.date) return -1;
+    return 0;
+}
+
+export const MomentFunc = input => moment(input).format('MMMM Do YYYY, h:mm a')
+
+export const searchWord = (input, search_word ) => input.toLowerCase().includes(search_word.toLowerCase())
