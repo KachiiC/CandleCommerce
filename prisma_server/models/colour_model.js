3da2668a9f17fbcb0d414c8bae617a38f252416c
Filippo Ceron <kaim.aragonar@gmail.com>
2022-05-01 19:39:42 +0200

    add colour model for generation and deletion logic

--- prisma_server/models/colour_model.js
+++ prisma_server/models/colour_model.js
@@ -1,14 +1,32 @@
-//router.post('/colors', async (req, res) => {
-//   console.log('body', req.body);
-//   const { colour, scents } = req.body;
-//   try {
-//     const newColor = await prisma.colour.create({
-//       data: { colour: colour, scents: { connect: scents } },
-//       include: { scents: true }
-//     });
-//     console.log('NC', newColor);
-//     res.status(201).send(newColor);
-//   } catch (error) {
-//     res.status(500).send('Function failed');
-//   }
-// });
+const Prisma = require('.');
+
+const addColour = (req) => {
+  const { colour, scents } = req;
+  try {
+    const newColor = await Prisma.colour.create({
+      data: {
+        colour: colour,
+        scents: {
+          connectOrCreate: scents.map(scent => {
+            return { where: { name: scent }, create: { name: scent } };
+          })
+        }
+      },
+      include: { scents: true }
+    });
+    res.status(201).send(newColor);
+  } catch (error) {
+    res.status(500).send('\nFailed in the model\n');
+  }
+}
+
+const deleteColour = (id) => {
+  try {
+    await Prisma.colour.delete({where: {id: +id}});
+    res.sendStatus(204);
+  } catch (err) {
+    res.status(500).send('\nFailed in the model\n');
+  }
+}
+
+module.exports = {addColour, deleteColour};
