97f3fe493d431cb88717d36f9803d5301d38e04f
Filippo Ceron <kaim.aragonar@gmail.com>
2022-05-02 21:36:14 +0200

    convert index file to ts

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,23 +2,20 @@ import { StrictMode } from 'react';
 import { render } from 'react-dom';
 import './index.css';
 import App from './App';
-import { Auth0Provider } from "@auth0/auth0-react";
+import { Auth0Provider } from '@auth0/auth0-react';
 
 const app = (
-
   <Auth0Provider
-    domain={process.env.REACT_APP_AUTH0_DOMAIN}
-    clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
+    domain={process.env.REACT_APP_AUTH0_DOMAIN!}
+    clientId={process.env.REACT_APP_AUTH0_CLIENT_ID!}
     redirectUri="http://127.0.0.1:3000"
   >
     <StrictMode>
       <App />
-    </StrictMode >
+    </StrictMode>
   </Auth0Provider>
-)
+);
 
-const root = document.getElementById('root')
+const root = document.getElementById('root');
 
 render(app, root);
-
-
