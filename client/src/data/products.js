6beb1b19436b6ee0ecaf518bd12230536e55a5ad
Filippo Ceron <kaim.aragonar@gmail.com>
2022-05-02 17:36:28 +0200

    fix broken picture link

--- client/src/data/products.js
+++ client/src/data/products.js
@@ -1,52 +1,56 @@
 const ProductsData = [
-    {
-        id: 1,
-        pictures: [
-            "https://s3-us-west-2.amazonaws.com/cdn.panda-gossips.com/production/posts/eyecatches/000/001/637/original.jpg?1515668442",
-            "https://cdn.shopify.com/s/files/1/0092/5096/3518/products/CA20025_4-TruGlow-Red-Slim-LED-Candles-Christmas_2000x2000.jpg?v=1596631539"
-        ],
-        title: "Lobster Red",
-        price: 18.99,
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        colours: "Lobster Red",
-        scents: "Baltic Amber"
-    },
-    {
-        id: 2,
-        pictures: [
-            "https://i.pinimg.com/originals/d0/7e/be/d07ebe7db6f2c6fdfcb308cef3e1df30.jpg",
-            "https://townsquare.media/site/393/files/2019/12/618OkzkoPGL._AC_SL1143_.jpg?w=980&q=75"
-        ],
-        title: "Kachi in spring",
-        price: 21.99,
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        colours: "White",
-        scents: "Fresh Linen"
-    },
-    {
-        id: 3,
-        pictures: [
-            "https://image.made-in-china.com/2f0j00gsOafERIJlqy/Scented-Candles-with-Glass-Jar-Yellow-Soybean-Candle-Wth-Jar-Glass.jpg",
-            "https://static.vecteezy.com/system/resources/previews/001/267/265/non_2x/stylish-yellow-candle-vector.jpg"
-        ],
-        title: "Tian Ah",
-        price: 8.99,
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        colours: "Black",
-        scents: "Bubblegum"
-    },
-    {
-        id: 4,
-        pictures: [
-            "https://craftlystudio.com/wp-content/uploads/2021/01/4-1.jpg",
-            "https://i.pinimg.com/originals/b3/fe/7e/b3fe7e1a035a2b4d299c8466b1e23f35.jpg"
-        ],
-        title: "Yellow Fever",
-        price: 12.99,
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        colours: "Yellow",
-        scents: "Pomegranate Noir"
-    }
-]
+  {
+    id: 1,
+    pictures: [
+      'https://s3-us-west-2.amazonaws.com/cdn.panda-gossips.com/production/posts/eyecatches/000/001/637/original.jpg?1515668442',
+      'https://cdn.shopify.com/s/files/1/0092/5096/3518/products/CA20025_4-TruGlow-Red-Slim-LED-Candles-Christmas_2000x2000.jpg?v=1596631539'
+    ],
+    title: 'Lobster Red',
+    price: 18.99,
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    colours: 'Lobster Red',
+    scents: 'Baltic Amber'
+  },
+  {
+    id: 2,
+    pictures: [
+      'https://i.pinimg.com/originals/d0/7e/be/d07ebe7db6f2c6fdfcb308cef3e1df30.jpg',
+      'https://townsquare.media/site/393/files/2019/12/618OkzkoPGL._AC_SL1143_.jpg?w=980&q=75'
+    ],
+    title: 'Kachi in spring',
+    price: 21.99,
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    colours: 'White',
+    scents: 'Fresh Linen'
+  },
+  {
+    id: 3,
+    pictures: [
+      'https://image.made-in-china.com/2f0j00gsOafERIJlqy/Scented-Candles-with-Glass-Jar-Yellow-Soybean-Candle-Wth-Jar-Glass.jpg',
+      'https://static.vecteezy.com/system/resources/previews/001/267/265/non_2x/stylish-yellow-candle-vector.jpg'
+    ],
+    title: 'Tian Ah',
+    price: 8.99,
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    colours: 'Black',
+    scents: 'Bubblegum'
+  },
+  {
+    id: 4,
+    pictures: [
+      'https://i.etsystatic.com/5961957/r/il/578f3e/3007109068/il_570xN.3007109068_ax1v.jpg',
+      'https://i.pinimg.com/originals/b3/fe/7e/b3fe7e1a035a2b4d299c8466b1e23f35.jpg'
+    ],
+    title: 'Yellow Fever',
+    price: 12.99,
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    colours: 'Yellow',
+    scents: 'Pomegranate Noir'
+  }
+];
 
-export default ProductsData
+export default ProductsData;
